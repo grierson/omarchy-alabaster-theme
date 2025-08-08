3e054598cd0b9f94c986dca5c67e42bf328a7ef3
Kyle Grierson <griersonky@gmail.com>
2025-08-08 10:53:58 +0000

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# omarchy-alabaster-theme
---
+++ neovim.lua
@@ -0,0 +1,12 @@
+return {
+	{
+		"p00f/alabaster.nvim",
+		priority = 1000,
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "alabaster",
+		},
+	},
+}
